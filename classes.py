834a85c31f56e4fde93d297d5d24526ba6568116
Hustini <ejdersalih02@gmail.com>
2024-06-04 18:35:56 +0200

    added necessary stuff for the animation

--- classes.py
+++ classes.py
@@ -5,9 +5,29 @@ import pygame
 class Player(pygame.sprite.Sprite):
     def __init__(self, x, y):
         super().__init__()
-        self._x = x
-        self._y = y
+        self.sprites = []
+        self.sprites.append(pygame.image.load('img/player/player1.png'))
+        self.sprites.append(pygame.image.load('img/player/player2.png'))
+        self.sprites.append(pygame.image.load('img/player/player3.png'))
+        self.sprites.append(pygame.image.load('img/player/player4.png'))
+        self.sprites.append(pygame.image.load('img/player/player5.png'))
+        self.sprites.append(pygame.image.load('img/player/player6.png'))
+        self.current_sprite = 0
+        self.image = self.sprites[self.current_sprite]
+
+        self.rect = self.image.get_rect()
+        self.rect.topleft = [x, y]
+
+        self.x = x
+        self.y = y
+
+    def update(self, speed):
+        self.current_sprite += speed
+        if self.current_sprite >= len(self.sprites):
+            self.current_sprite = 0
+        self.image = self.sprites[int(self.current_sprite)]
 
 
 if __name__ == '__main__':
-    pass
+    player = Player(10, 10)
+    print(player)
