e04a086083d78936bf3016b125823d00f1f92843
Hustini <ejdersalih02@gmail.com>
2024-06-06 18:47:56 +0200

    draw the pillar on the screen

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 import sys
 from classes import Player
+from classes import Pillar
 
 # Initialize Pygame
 pygame.init()
@@ -16,14 +17,21 @@ BG = pygame.transform.scale(pygame.image.load('./img/background.jpeg'), (screen_
 
 # Define player and sprite group for animation
 moving_sprites = pygame.sprite.Group()
-player = Player(10, 10)
+player = Player(100, 100)
 moving_sprites.add(player)
+
+# sprite group for the pillar
+pillar_sprite = pygame.sprite.Group()
+pillars = [Pillar(-50, 0), Pillar(-50, 400), Pillar(370, 0), Pillar(370, 400)]
+for i in pillars:
+    pillar_sprite.add(i)
 
 
 def draw_window():
     screen.blit(BG, (0, 0))
     moving_sprites.draw(screen)
-    moving_sprites.update(0.0035)
+    moving_sprites.update(0.009)
+    pillar_sprite.draw(screen)
     pygame.display.flip()
 
 
